d047697e776200cf91c711e8ca5521e308e7da3a
charleshuynh <charleshuynh.connect@gmail.com>
2024-06-13 00:33:18 +0700

    check request api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/search', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     })
     const page = await browser.newPage()
