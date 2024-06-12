e4a274b2eb88583a46a4138e02479bfa1962a2ab
charleshuynh <charleshuynh.connect@gmail.com>
2024-06-13 00:11:18 +0700

    check error server

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.get('/search', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
+      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     })
     const page = await browser.newPage()
